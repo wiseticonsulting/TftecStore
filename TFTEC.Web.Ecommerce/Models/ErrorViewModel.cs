638f5e42e96334c40182b2d13b20b8dad068e834
Guilherme Campos <guilherme.campos@tftec.com.br>
2022-10-04 16:02:38 -0300

    Primeiro checkin do bem ecommerce

---
+++ TFTEC.Web.Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TFTEC.Web.Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
