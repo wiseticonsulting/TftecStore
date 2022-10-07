88889642a65d0e18cfde049120a42bbb4ddc721a
Guilherme Campos <guilherme.campos@tftec.com.br>
2022-10-07 17:04:48 -0300

    Implementação completa do azure key vault com banco de dados

--- TFTEC.Web.Ecommerce/Program.cs
+++ TFTEC.Web.Ecommerce/Program.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 
 namespace TFTEC.Web.Ecommerce;
 public class Program
@@ -10,7 +14,21 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+        .ConfigureAppConfiguration((context, config) =>
+        {
+            var buildConfig = config.Build();
+
+            string kvURL = buildConfig["KeyVaultTFTEC:KVUrl"];
+            string tenantId = buildConfig["KeyVaultTFTEC:TenantId"];
+            string clientId = buildConfig["KeyVaultTFTEC:ClientId"];
+            string clientSecret = buildConfig["KeyVaultTFTEC:ClientSecretId"];
+
+            var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);  
+
+            var keyVaultEndpoint = new SecretClient(new Uri(kvURL), credential);
+            config.AddAzureKeyVault(keyVaultEndpoint, new AzureKeyVaultConfigurationOptions());
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
